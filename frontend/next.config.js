86135abb7b06dd62f2e6841ae3beace67a9dc9b7
Bill Adams <vikingbill@gmail.com>
2022-05-29 18:57:44 -0400

    get redux auth working with nextjs via swr

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,12 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/:path*' // Proxy to Backend
-      }
-    ]
+    if (process.env.NODE_ENV !== 'production') {
+
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:5000/api/:path*' // Proxy to Backend
+        }
+      ]
+    } else return []
   },
 }
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
 import defaultStore from '../store'
+import { useEffect } from 'react'
+import useSWR from 'swr'
+import cookieFetch from '../utilities/cookieFetch'
+
 
 function MyApp({ Component, pageProps }) {
+  if (process.env.NODE_ENV !== 'production') {
+    // cookieFetch('https://localhost:5000/api/csrf/restore')
+    useSWR('/api/csrf/restore', cookieFetch)
+  }
+  // useEffect(() => {
+  //   if (process.env.NODE_ENV !== 'production') {
+  //     fetch('https://localhost:5000/api/csrf/restore')
+  //   }
+  // }, [])
   const store = defaultStore()
   return (
     <Provider store={store}>
